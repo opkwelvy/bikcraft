e093d47f0dc2225fc75447c697796b46a8cfd984
Kelvy Correa <kelvycorrea35@gmail.com>
2022-12-11 13:40:46 -0300

    Adição de galeria em JS

--- js/script.js
+++ js/script.js
@@ -20,3 +20,38 @@ function ativarProduto(parametro){
   }
 }
 parametros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+const perguntas =document.querySelectorAll('.perguntas button');
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls)
+  resposta.classList.toggle('ativa')
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttributes("aria-expanded", ativa)
+
+}
+function eventosPerguntas(perguntas){
+  perguntas.addEventListener('click', ativarPergunta)
+}
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de imagens
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  console.log(media);
+  if (media){
+    console.log(media);
+    galeriaContainer.prepend(img);
+  }
+}
+function eventosGaleria(img){
+  img.addEventListener("click", trocarImagem);
+}
+galeria.forEach(eventosGaleria);
